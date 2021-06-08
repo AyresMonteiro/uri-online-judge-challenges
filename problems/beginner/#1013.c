4903d2b2074ea3d5c4db5999e07ad32b82f58866
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-06-08 15:38:51 -0300

    the greatest

---
+++ problems/beginner/#1013.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int * readNumbers() {
+	int * n = (int*) malloc(sizeof(int) * 3);
+	scanf("%d %d %d", n, n + 1, n + 2);
+	return n;
+}
+
+int maior (int a, int b) {
+	return (a + b + abs(a - b))/2;
+}
+
+int main () {
+	int * n = readNumbers();
+	printf("%d eh o maior\n", maior(maior(n[0], n[1]), n[2]));
+	free(n);
+	return 0;
+}
+
