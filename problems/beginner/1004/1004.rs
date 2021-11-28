170c2b758818be269c1a235b8ff3da823e8d3995
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-11-27 21:39:13 -0300

    rust: 1004 - simple product

---
+++ problems/beginner/1004/1004.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn get_number_from_stdin() -> i32 {
+	let mut num = String::new();
+
+	io::stdin()
+		.read_line(&mut num)
+		.expect("Error while reading from stdin");
+
+	let num: i32 = num
+		.trim()
+		.parse()
+		.expect("Error while parsing string into number");
+
+	num
+}
+
+fn main() {
+	let a = get_number_from_stdin();
+	let b = get_number_from_stdin();
+
+	println!("PROD = {}", a * b);
+}
