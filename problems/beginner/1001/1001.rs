5e2aa307e328e51cdebf28e3b31cda20d39f2c2c
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-11-27 21:21:49 -0300

    rust: 1001 - extremely basic

---
+++ problems/beginner/1001/1001.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn get_number_from_stdin() -> i32 {
+	let mut num = String::new();
+	
+	io::stdin()
+		.read_line(&mut num)
+		.expect("Error while reading");
+	
+	let num: i32 = num.trim().parse().expect("Error while parsing number");
+
+	num
+}
+
+
+fn main() {
+	let a = get_number_from_stdin();
+	let b = get_number_from_stdin();
+
+	println!("X = {}", a + b);
+}
