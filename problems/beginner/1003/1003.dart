d0528da259f93d5590d26866a4d5837a4e8786c5
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-03 19:36:22 -0300

    dart: 1003 - simple sum

---
+++ problems/beginner/1003/1003.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
+String input() {
+  return (stdin.readLineSync(encoding: utf8) as String);
+}
+
+int intInput() {
+  return int.parse(input().trim());
+}
+
+void main() {
+  var a = intInput();
+  var b = intInput();
+
+  print('SOMA = ${a + b}');
+}
