5b49a82a873bfbd213f0d6c3f586cb1e6f58034f
AyresMonteiro <main@ayresmonteiro.com>
2023-11-10 07:54:13 -0300

    c: 1096 - sequence ij 2

---
+++ problems/beginner/1096/1096.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    for (int i = 1, j = 7; i < 10; j--) {
+        printf("I=%i J=%i\n", i, j);
+
+        if (j == 5) {
+            j = 8;
+            i += 2;
+        }
+    }
+
+    return 0;
+}
